983b29abb75f503bd93bc990500644d5a892101b
shinv <shin-production@mail.ru>
2025-09-04 10:23:10 +0200

    Supprimer tout les todos , tout fonctionne

--- src/app/features/todos/components/todo-list.page.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  standalone: true,
-  selector: 'app-todo-list',
-  template: `<h1 class="text-2xl font-bold">Mes Todos</h1>`,
-})
-export class TodoListPage {}
--- src/app/features/todos/todos.routes.ts
+++
@@ -1,3 +0,0 @@
-import { Routes } from '@angular/router';
-import { TodoListPage } from './components/todo-list.page';
-export const TODOS_ROUTES: Routes = [{ path: '', component: TodoListPage }];
